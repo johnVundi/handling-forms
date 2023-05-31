9598c018b721f51c3f34efd9b47f2e86803ebd73
John Vundi <johnsonvundy2@gmail.com>
2023-05-31 15:37:08 +0300

    configured how to create a book

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function BookCreate() {
+function BookCreate({onCreate}) {
+    const [tittle, setTittle] = useState('')
+
+    const handleChange = (event) =>{
+        setTittle(event.target.value)
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        onCreate(tittle)
+        setTittle('')
+    }
+
   return (
-    <div>BookCreate</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label>Tittle</label>
+            <input value={tittle} onChange={handleChange} />
+            <button>submit</button>
+        </form>
+    </div>
   )
 }
 
